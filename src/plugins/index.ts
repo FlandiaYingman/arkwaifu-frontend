f66f9aa179e693333ed0cf1b726e3d29843f7c63
Flandia Yingman <flandia_yingm@hotmail.com>
2023-05-19 02:58:11 +0800

    style: reformat files from scaffold

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -13,7 +13,7 @@ import router from '../router'
 // Types
 import type { App } from 'vue'
 
-export function registerPlugins (app: App) {
+export function registerPlugins(app: App) {
   loadFonts()
   app
     .use(vuetify)
