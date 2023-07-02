77469552d7ef4d1a3ff1da15e0c1e7549a5b3b8a
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-02 21:03:45 +0800

    feat: i18nize AboutView

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,22 +3,21 @@
     <p>
       <v-img src="@/assets/logo_phantom.png" width="256" />
     </p>
-    <p class="text-h3">
-      About Arkwaifu...
-    </p>
-    <p class="text-body-1">
-      Arkwaifu is a website which, arranges and provides almost all artworks extracted from <b>Arknights</b> (the
-      game). Arkwaifu also enlarges (4x) the artworks with super-resolution neural networks - Real-ESRGAN.
-    </p>
+    <i18n-t keypath="title" tag="p" class="text-h3" />
+    <i18n-t keypath="p1" tag="p" class="text-body-1">
+      <template #arknights><b>{{ t('arknights') }}</b></template>
+    </i18n-t>
     <p>
       <v-avatar image="@/assets/FlandiaYingman.png" size="192" />
     </p>
-    <p class="text-body-1">
-      I am Flandia Yingman (<a href="https://github.com/FlandiaYingman">@FlandiaYingman</a>), the webmaster and
-      currently the sole maintainer of Arkwaifu. If you have any questions, or want to contribute to Arkwaifu, feel
-      free to create an issue or pull request to <a href="https://github.com/FlandiaYingman/arkwaifu">the repository</a>.
-      Any questions, comments, suggestions and contribution are welcome!
-    </p>
+    <i18n-t keypath="p2" tag="p" class="text-body-1">
+      <template #flandia_yingman>
+        <a href="https://github.com/FlandiaYingman">@FlandiaYingman</a>
+      </template>
+      <template #the_repository>
+        <a href="https://github.com/FlandiaYingman/arkwaifu">{{ t('the_repository') }}</a>
+      </template>
+    </i18n-t>
   </v-container>
 </template>
 
@@ -28,5 +27,41 @@
     line-height: 1.8;
   }
 </style>
+
 <script setup lang="ts">
+  import { useI18n } from 'vue-i18n'
+
+  const { t } = useI18n()
 </script>
+
+<i18n locale="en" lang="yaml">
+arknights: >-
+  Arknights
+the_repository: >-
+  the repository
+title: >-
+  About Arkwaifu...
+p1: >-
+  Arkwaifu is a website which arranges and provides almost all artworks extracted from {arknights} (the game).
+  Arkwaifu also enlarges (4x) the artworks with super-resolution neural networks - Real-ESRGAN.
+p2: >-
+  I am Flandia Yingman ({flandia_yingman}), the webmaster and currently the sole maintainer of Arkwaifu.
+  If you have any questions or want to contribute to Arkwaifu, feel free to create an issue or pull request to {the_repository}.
+  Any questions, comments, suggestions and contribution are welcome!
+</i18n>
+
+<i18n locale="zh" lang="yaml">
+arknights: >-
+  明日方舟
+the_repository: >-
+  这个仓库
+title: >-
+  关于 Arkwaifu……
+p1: >-
+  Arkwaifu 提供几乎所有从{arknights}中提取的图片资源。
+  同时，Arkwaifu 也会利用神经网络 Real-ESRGAN 将资源进行超分辨率（4x）。
+p2: >-
+  我是 Flandia Yingman（{flandia_yingman}），Arkwaifu 的站长和目前唯一的开发者。
+  如果有任何问题，或者想为 Arkwaifu 贡献，就来{the_repository}创建一个 issue 或者 pull request 吧。
+  任何问题、建议或者贡献都可以！
+</i18n>
