b53422a738cf9bb57f130fe3c2e534ecf140d0ec
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-11 13:34:45 +0800

    feat(ui): redesign server and language switcher to adapt mobile devices

--- src/layouts/default/DefaultView.vue
+++ src/layouts/default/DefaultView.vue
@@ -6,17 +6,53 @@
       <v-app-bar-nav-icon @click="drawerOpen = !drawerOpen" />
       <v-toolbar-title>Arkwaifu</v-toolbar-title>
 
-      <v-btn-toggle v-model="serverNum" mandatory color="secondary" class="mr-2">
-        <v-btn v-for="server in Object.keys(Server)" prepend-icon="mdi-server">
-          {{ server }}
-        </v-btn>
-      </v-btn-toggle>
+      <v-btn
+        v-if="mdAndUp"
+        id="server-menu-activator"
+        prepend-icon="mdi-server"
+      >
+        {{ t('server') }}
+      </v-btn>
+      <v-btn
+        v-else
+        id="server-menu-activator"
+        icon="mdi-server"
+      />
+      <v-menu activator="#server-menu-activator">
+        <v-list>
+          <v-list-item
+            v-for="server in Object.keys(Server)"
+            @click="api.switchServer(Server[server])"
+            prepend-icon="mdi-server"
+          >
+            {{ server }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-      <v-btn-toggle v-model="langNum" mandatory color="secondary" class="mr-2">
-        <v-btn v-for="locale in Object.keys(Locale)" prepend-icon="mdi-translate">
-          {{ (new Intl.DisplayNames([locale], { type: 'language' })).of(locale) }}
-        </v-btn>
-      </v-btn-toggle>
+      <v-btn
+        v-if="mdAndUp"
+        id="language-menu-activator"
+        prepend-icon="mdi-translate"
+      >
+        {{ t('language') }}
+      </v-btn>
+      <v-btn
+        v-else
+        id="language-menu-activator"
+        icon="mdi-translate"
+      />
+      <v-menu activator="#language-menu-activator">
+        <v-list>
+          <v-list-item
+            v-for="locale in Object.keys(Locale)"
+            @click="$i18n.locale = Locale[locale]"
+            prepend-icon="mdi-translate"
+          >
+            {{ (new Intl.DisplayNames([locale], { type: 'language' })).of(locale) }}
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
       <template v-slot:append>
         <v-btn class="mx-2" icon="mdi-dots-vertical"></v-btn>
@@ -32,24 +68,20 @@
 </template>
 
 <script setup lang="ts">
-  import { computed, ref, watchPostEffect } from 'vue'
+  import { ref } from 'vue'
   import NavigationDrawer from '@/components/NavigationDrawer.vue'
   import { Server, useApi } from '@/arkwaifu-api'
   import { useI18n } from 'vue-i18n'
   import { Locale } from '@/locales'
+  import { useDisplay } from 'vuetify'
 
   const api = useApi()
   const i18n = useI18n()
+  const { t } = i18n
 
   const drawerOpen = ref<boolean | null>(null)
 
-  const serverNum = ref(Object.keys(Server).indexOf(api.server))
-  const server = computed<Server>(() => Server[Object.keys(Server)[serverNum.value] as keyof typeof Server])
-  watchPostEffect(() => api.switchServer(server.value))
-
-  const langNum = ref(Object.values(Locale).indexOf(i18n.locale.value as Locale))
-  const lang = computed<Locale>(() => Locale[Object.keys(Locale)[langNum.value] as keyof typeof Locale])
-  watchPostEffect(() => i18n.locale.value = lang.value)
+  const { mdAndUp } = useDisplay()
 </script>
 
 <style lang="scss">
@@ -58,3 +90,13 @@
     scroll-padding-top: (64px + 32px + 16px);
   }
 </style>
+
+<i18n locale="en" lang="yaml">
+server: Server
+language: Language
+</i18n>
+
+<i18n locale="zh" lang="yaml">
+server: 服务器
+language: 语言
+</i18n>
