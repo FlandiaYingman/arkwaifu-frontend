ffa0c9820328a05a5f9acab0827e90f7320af6f9
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-15 23:57:59 +0800

    feat(ui): redesign GroupSheet

--- src/components/story/GroupSheet.vue
+++ src/components/story/GroupSheet.vue
@@ -1,32 +1,50 @@
 <template>
-  <!-- the height is an estimated value for lazy load -->
-  <v-lazy :min-height="72 + 16 * 2 + group.stories.length * 120 / 6">
-    <v-container>
-      <p :id="group.id" class="text-h5"><a :href="`#${group.id}`" class="text-black text-decoration-none">§</a>
-        {{ group.name }}</p>
-      <p class="text-caption text-medium-emphasis">{{ group.type }}</p>
-      <p class="text-caption text-medium-emphasis">{{ group.id }}</p>
-      <v-row class="mx-1 my-2">
-        <v-col cols="6" sm="4" lg="3" xl="2">
-          <group-card :group="group" :focused="focus?.id == group.id" />
-        </v-col>
-        <v-col v-for="story in group.stories" cols="6" sm="4" lg="3" xl="2">
-          <story-card :story="story" :focused="focus?.id == story.id" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-lazy>
+  <v-container>
+    <p :id="group.id" class="text-h5">
+      <a :href="`#${group.id}`" class="text-black text-decoration-none">§</a>
+      {{ group.name }}
+    </p>
+    <p class="text-caption text-medium-emphasis">
+      {{ t(group.type) }}
+    </p>
+    <p class="text-caption text-medium-emphasis">
+      {{ group.id }}
+    </p>
+    <v-row class="mx-1 my-2">
+      <v-col v-if="group.stories.length > 1" cols="6" sm="4" lg="3" xl="2">
+        <group-card :group="group" :focused="focus?.id == group.id" />
+      </v-col>
+      <v-col v-for="story in group.stories" cols="6" sm="4" lg="3" xl="2">
+        <story-card :story="story" :focused="focus?.id == story.id" />
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
   import { Story, StoryGroup } from '@/arkwaifu-api'
   import StoryCard from '@/components/story/StoryCard.vue'
   import GroupCard from '@/components/story/GroupCard.vue'
+  import { useI18n } from 'vue-i18n'
 
   defineProps<{
     group: StoryGroup,
     focus?: StoryGroup | Story,
   }>()
+
+  const { t } = useI18n()
 </script>
 
+<i18n locale="en" lang="yaml">
+main-story: Main Theme
+major-event: Intermezzo / Side Story
+minor-event: Vignette
+other: Other
+</i18n>
 
+<i18n locale="zh" lang="yaml">
+main-story: 主题曲
+major-event: 插曲 & 别传
+minor-event: 故事集
+other: 其它
+</i18n>
