59caf0a22ceaa80e89906b5eef182977b86394a3
Flandia Yingman <flandia_yingm@hotmail.com>
2023-07-27 00:33:45 +0800

    feat(api): use ref instead of manually update server

--- src/stores/arkwaifu-api.ts
+++ src/stores/arkwaifu-api.ts
@@ -1,48 +1,52 @@
 import { defineStore } from 'pinia'
-import { Art, Server, Variation } from '@/arkwaifu-api'
-import { AggregatedCharacterArt, AggregatedPictureArt, Story, StoryGroup } from '@/arkwaifu-api'
+import { AggregatedCharacterArt, AggregatedPictureArt, Art, Server, Story, StoryGroup, Variation } from '@/arkwaifu-api'
+import { type Ref, ref } from 'vue'
 
 export const useArkwaifu = defineStore('arkwaifu-api', () => {
   const api = import.meta.env.VITE_ARKWAIFU_API_BASE_URL ?? '/api/v1'
-  const server = JSON.parse(localStorage.getItem('server') ?? 'null') ?? Server.CN
+
+  const server: Ref<Server> = (() => {
+    const localServer = localStorage.getItem('server')
+    if (localServer) {
+      return ref(JSON.parse(localServer))
+    } else {
+      return ref(Server.CN)
+    }
+  })()
 
   function switchServer(newServer: Server) {
-    if (newServer != server) {
-      localStorage.setItem('server', JSON.stringify(newServer))
-      location.reload()
-    }
+    server.value = newServer
   }
 
   async function fetchStoryGroupsByType(type: string): Promise<StoryGroup[]> {
-    return fetch(`${api}/${server}/story-groups?type=${type}`)
-      .then((el) => el.json())
-      .then((el) => el as any[])
-      .then((el) => el.map((obj) => Object.assign(new StoryGroup(), obj)))
+    const resp = await fetch(`${api}/${server.value}/story-groups?type=${type}`)
+    const jsonObj: any[] = await resp.json()
+    return jsonObj.map((el) => Object.assign(new StoryGroup(), el))
   }
 
   async function fetchStoryGroupByID(id: string): Promise<StoryGroup> {
     id = encodeURIComponent(id)
-    return fetch(`${api}/${server}/story-groups/${id}`)
-      .then((el) => el.json())
-      .then((el) => Object.assign(new StoryGroup(), el))
+    const resp = await fetch(`${api}/${server.value}/story-groups/${id}`)
+    const jsonObj: any = resp.json()
+    return Object.assign(new StoryGroup(), jsonObj)
   }
 
   async function fetchStoryByID(id: string): Promise<Story> {
     id = encodeURIComponent(id)
-    return fetch(`${api}/${server}/stories/${id}`)
-      .then((el) => el.json())
-      .then((el) => Object.assign(new Story(), el))
+    const resp = await fetch(`${api}/${server.value}/stories/${id}`)
+    const jsonObj: any = resp.json()
+    return Object.assign(new Story(), jsonObj)
   }
 
   async function fetchAggregatedPictureArt(id: string): Promise<AggregatedPictureArt> {
-    const resp = await fetch(`${api}/${server}/aggregated-picture-arts/${id}`)
+    const resp = await fetch(`${api}/${server.value}/aggregated-picture-arts/${id}`)
     const jsonObj = await resp.json()
     return Object.assign(new AggregatedPictureArt(), jsonObj)
   }
 
   async function fetchAggregatedCharacterArt(id: string): Promise<AggregatedCharacterArt> {
     id = encodeURIComponent(id)
-    const resp = await fetch(`${api}/${server}/aggregated-character-arts/${id}`)
+    const resp = await fetch(`${api}/${server.value}/aggregated-character-arts/${id}`)
     const jsonObj = await resp.json()
     return Object.assign(new AggregatedCharacterArt(), jsonObj)
   }
@@ -54,19 +58,19 @@ export const useArkwaifu = defineStore('arkwaifu-api', () => {
   }
 
   async function fetchArtsOfStoryGroup(groupID: string): Promise<Art[]> {
-    const resp = await fetch(`${api}/arts?server=${server}&group=${groupID}`)
+    const resp = await fetch(`${api}/arts?server=${server.value}&group=${groupID}`)
     const jsonObj = await resp.json()
     return jsonObj.map((el: any) => Object.assign(new Art(), el))
   }
 
   async function fetchArtsOfStory(storyID: string): Promise<Art[]> {
-    const resp = await fetch(`${api}/arts?server=${server}&story=${storyID}`)
+    const resp = await fetch(`${api}/arts?server=${server.value}&story=${storyID}`)
     const jsonObj = await resp.json()
     return jsonObj.map((el: any) => Object.assign(new Art(), el))
   }
 
   async function fetchArtsExceptForStoryArts(): Promise<Art[]> {
-    const resp = await fetch(`${api}/arts?server=${server}&except-for-story-arts=true`)
+    const resp = await fetch(`${api}/arts?server=${server.value}&except-for-story-arts=true`)
     const jsonObj = await resp.json()
     return jsonObj.map((el: any) => Object.assign(new Art(), el))
   }
