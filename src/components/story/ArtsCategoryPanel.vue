218feec4be81df1d3bdff16e0df218e37ca618ba
Flandia Yingman <me@flandia.dev>
2024-01-21 00:42:11 +0800

    feat: support navigating carousel with keyboard (left, right, esc)

--- src/components/story/ArtsCategoryPanel.vue
+++ src/components/story/ArtsCategoryPanel.vue
@@ -5,18 +5,18 @@
       {{ category }}
     </p>
     <v-row class="mx-1 my-2">
-      <v-col v-for="art in arts" :key="art.id" cols="6" lg="3" sm="4" xl="2">
-        <art-card :art="art" @loupe="openCarousel(art.id)" />
+      <v-col v-for="(art, i) in arts" :key="art.id" cols="6" lg="3" sm="4" xl="2">
+        <art-card :art="art" @loupe="openCarousel(i)" />
       </v-col>
     </v-row>
-    <v-overlay v-model="carouselOverlayModel" class="align-center justify-center">
+    <v-overlay ref="carouselOverlay" v-model="carouselOverlayModel" class="align-center justify-center">
       <v-sheet>
         <v-carousel v-model="carouselModel" height="100%" hide-delimiters show-arrows="hover">
           <v-carousel-item
-            v-for="art in arts"
+            v-for="(art, i) in arts"
             :key="art.id"
             :src="api.contentSrcOf(art.id, Variation.Origin)"
-            :value="art.id"
+            :value="i"
             :width="sizeFittingBox(art.variants[0].contentWidth, art.variants[0].contentHeight).value[0]"
             :height="sizeFittingBox(art.variants[0].contentWidth, art.variants[0].contentHeight).value[1]"
             eager
@@ -31,7 +31,7 @@
 <script lang="ts" setup>
 import { Art, Variation } from '@/arkwaifu-api'
 import ArtCard from '@/components/art/ArtCard.vue'
-import { computed, ref } from 'vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 import { useArkwaifu } from '@/stores/arkwaifu-api'
 import { useDisplay } from 'vuetify'
 
@@ -48,16 +48,48 @@ function sizeFittingBox(wi: number, hi: number) {
   })
 }
 
-defineProps<{
+const props = defineProps<{
   arts: Art[]
   category: string
 }>()
 
 const carouselOverlayModel = ref<boolean>(false)
-const carouselModel = ref<string>('')
+const carouselModel = ref<number>(0)
 
-function openCarousel(newCarouselValue: string) {
+// Keyboard Listeners
+
+function openCarousel(newCarouselValue: number) {
   carouselModel.value = newCarouselValue
   carouselOverlayModel.value = true
 }
+
+function switchCarouselLeft() {
+  if (carouselModel.value === 0) {
+    carouselModel.value = props.arts.length - 1
+  } else {
+    carouselModel.value = carouselModel.value - 1
+  }
+}
+
+function switchCarouselRight() {
+  if (carouselModel.value === props.arts.length - 1) {
+    carouselModel.value = 0
+  } else {
+    carouselModel.value = carouselModel.value + 1
+  }
+}
+
+function closeCarousel() {
+  carouselOverlayModel.value = false
+}
+
+const KeyboardListener = (e: KeyboardEvent) => {
+  if (carouselOverlayModel.value) {
+    if (e.key === 'Escape') closeCarousel()
+    else if (e.key === 'ArrowLeft') switchCarouselLeft()
+    else if (e.key === 'ArrowRight') switchCarouselRight()
+  }
+}
+onMounted(() => window.addEventListener('keydown', KeyboardListener))
+onUnmounted(() => window.removeEventListener('keydown', KeyboardListener))
 </script>
